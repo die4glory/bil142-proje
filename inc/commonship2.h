36d611be068422333ccab95bc07499f3d540d19f
die4glory <156427768+die4glory@users.noreply.github.com>
2024-03-17 17:53:52 +0300

    Add files via upload

---
+++ inc/commonship2.h
@@ -0,0 +1,21 @@
+
+#ifndef UNTITLED_COMMONSHIP2_H
+#define UNTITLED_COMMONSHIP2_H
+#include "spaceships.h"
+
+class commonShip:public Spaceships {
+public:
+    commonShip():Spaceships(1){}
+
+    int updateHealthDamage10(int& _health) override;
+    int updateHealthDamage30(int& _health) override;
+    void asteroid() override;
+    void run() override;
+    void fight() override;
+
+private:
+protected:
+
+};
+
+#endif //UNTITLED_COMMONSHIP2_H
